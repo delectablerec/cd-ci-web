dd76052ecfdeb03e593250b7505327190d10c5cb
delectablerec <info@delectablerecords.com>
2025-02-28 09:48:54 +0100

    data-vis

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -7,11 +7,15 @@ using System.Threading.Tasks;
 public class IndexModel : PageModel
 {
     public List<Prodotto> Prodotti { get; set; } = new();
+    public string? Environment { get; set; }
+    public string? filePath { get; set; }
 
     public async Task OnGet()
     {
-        var filePath = Environment.GetEnvironmentVariable("PRODOTTI_JSON_PATH") ?? "wwwroot/data/prodotti.json";
+        filePath = Environment.GetEnvironmentVariable("PRODOTTI_JSON_PATH") ?? "wwwroot/data/prodotti.json";
         // var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/data/prodotti.json");
+        Environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+
         if (System.IO.File.Exists(filePath))
         {
             var json = await System.IO.File.ReadAllTextAsync(filePath);
